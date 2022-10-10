fa7d26d835a6b5a31d69ac33d85f1349f548faa6
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-10-10 11:00:22 -0500

    define MyComponent

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    {{data}}
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: ["data"],
+  beforeMount() {
+    console.log(this.data)
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: #0022AA;
+}
+</style>
